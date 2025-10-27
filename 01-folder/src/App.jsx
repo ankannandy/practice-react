ec9418648fc97b203dd42bbcd876c3b476447130
Ankan Nandy <ankannandy03@gmail.com>
2025-10-27 15:41:32 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='cls1'>
+        <h1>app</h1>
+        <h2>hello</h2>
+      </div>
+      <div id='cls2'>
+        <h3>hii</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,12 @@
+function card(){
+
+    return (
+              <div className='card'>
+                <h1>Ankan</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, molestias.</p>
+              </div>
+    )
+
+}
+
+export default card;
