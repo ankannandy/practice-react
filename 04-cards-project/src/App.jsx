704a6db9b45ac41b475d547126440586989ccb9e
Ankan Nandy <ankannandy03@gmail.com>
2025-12-02 15:28:23 +0530

    upolod

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Bookmark } from 'lucide-react'
 
 const App = () => {
   return (
     <div className="parent">
-      app test
+      <div className="card">
+        <div className="top">
+          <img src="https://static.vecteezy.com/system/resources/previews/014/018/561/non_2x/amazon-logo-on-transparent-background-free-vector.jpg" alt=" logo" />
+          <button>Save <Bookmark /></button>
+        </div>
+        <div className="center">
+          <h3>amazon <span>5 Days ago</span></h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div>
+            <h4>Part Time</h4>
+            <h4>Senior level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <div>
+              <h3>$125/hours</h3>
+              <p>Mumbai</p>
+            </div>
+            <button>Apply Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
